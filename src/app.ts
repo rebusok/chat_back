5788868809e1509a253e8d9fe2076221feacd0bf
Yuriy <rebusok@yandex.ru>
2021-04-08 17:23:00 +0300

    add name

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import http from 'http';
-import {Server }  from 'socket.io';
-const cors = require('cors');
+import {Server} from 'socket.io';
+
 const app = express();
 
+const messages:Array<any> = [
 
+]
+const users = new Map()
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -19,9 +22,32 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+    users.set(socket, {id: new Date().getTime().toString(), name: 'Anonym'})
+    socket.on('client-name-sent', (name) => {
+        if (typeof name !== 'string') return
+        const user = users.get(socket)
+        user.name = name
+
+    })
+    socket.on('disconnect', () => {
+        users.delete(socket)
+    });
+    socket.on('client-message-sent', mes => {
+        if (typeof mes !== 'string') return
+        const user = users.get(socket)
+        let mesItem = {
+            message: mes, id: new Date().getTime().toString(),
+            user: {id: user.id, name: user.name}
+        }
+        messages.push(mesItem)
+        socket.emit('new-message-sent', mesItem)
+    })
+    socket.emit('messages-init', messages)
+
     console.log('a user connected');
 });
 
+
 server.listen(PORT, () => {
-    console.log('listening on *:3000');
+    console.log('listening on *:3009');
 });

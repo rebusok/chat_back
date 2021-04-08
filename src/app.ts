ad6e9c9ff5daf8bb4c0eb1c5bc5c1704ef51ef08
Yuriy <rebusok@yandex.ru>
2021-04-08 14:59:08 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import http from 'http';
 import {Server }  from 'socket.io';
+const cors = require('cors');
 const app = express();
+
+app.use(cors())
 const server = http.createServer(app);
 const io = new Server(server)
 

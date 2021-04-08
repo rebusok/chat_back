daebef8d9e49b44a835c8e299d0dada9b237d5db
Yuriy <rebusok@yandex.ru>
2021-04-08 15:09:08 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,12 @@ const app = express();
 
 
 const server = http.createServer(app);
-const io = new Server(server, );
-// {
-//     cors: {
-//         origin: '*',
-//     }
-// }
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
 
-io.use(cors())
 const PORT = process.env.PORT || 3009
 
 app.get('/', (req, res) => {
